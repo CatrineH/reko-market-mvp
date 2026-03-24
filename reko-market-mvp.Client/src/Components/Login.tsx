85559475ceea73abd04ced9c077c1e1a374b378b
Nutelline <143810394+Nutelline@users.noreply.github.com>
2026-03-24 13:24:57 +0100

    Added react-router package and login input fields

--- reko-market-mvp.Client/src/Components/Login.tsx
+++ reko-market-mvp.Client/src/Components/Login.tsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+
 function Login() {
+    const [username, setUsername] = useState ("");
+    const [password, setPassword] = useState (""); 
 
   return (
     <>
     <h1>Hello Login</h1>
+
+    <form>
+        <div>
+            <label>Username</label>
+            <input 
+            type="text"
+            value={username}
+            onChange={(e) => setUsername (e.target.value)}
+            />
+        </div>
+
+    <div>
+        <label>Password</label>
+          <input
+            type="text"
+            value={password}
+            onChange={(e) => setPassword (e.target.value)}
+         />
+    </div>
+
+    <button type="submit">Logg inn</button>
+
+    </form>
     </>
   )
 }
