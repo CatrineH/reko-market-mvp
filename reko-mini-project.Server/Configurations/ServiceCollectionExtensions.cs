00c27f047d4f5ad32ca5d48de1066265aeb18b6c
doccelick <anders.e.bakke@gmail.com>
2026-03-24 12:45:49 +0100

    Add database configuration setup using SQLite

--- reko-mini-project.Server/Configurations/ServiceCollectionExtensions.cs
+++ reko-mini-project.Server/Configurations/ServiceCollectionExtensions.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using reko_mini_project.Server.Data;
+
 namespace reko_mini_project.Server.Configurations;
 
 public static class ServiceCollectionExtensions
 {
     public const string FRONTEND_CORS_POLICY = "FRONTEND_CORS_POLICY";
     public const string ALLOWED_ORIGINS_CONFIG_KEY = "ALLOWED_ORIGINS";
+    public const string DATABASE_CONNECTION = "DefaultConnection";
 
     public static IServiceCollection AppApplicationServices(
         this IServiceCollection services,
@@ -11,6 +15,7 @@ public static class ServiceCollectionExtensions
     {
         services.AddOpenApi();
         services.AddCorsConfiguration(configuration);
+        services.AddDatabaseConfiguration(configuration);
 
         return services;
     }
@@ -32,6 +37,15 @@ public static class ServiceCollectionExtensions
             });
         });
 
+        return services;
+    }
+
+    public static IServiceCollection AddDatabaseConfiguration(
+    this IServiceCollection services,
+    IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlite(configuration.GetConnectionString(DATABASE_CONNECTION)));
         return services;
     }
 }
