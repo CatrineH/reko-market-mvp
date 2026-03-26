96d85ba37a8d547474c5089543e97fa453ce88f2
doccelick <anders.e.bakke@gmail.com>
2026-03-26 10:18:43 +0100

    Add ImageUrl property to Product class

--- reko-mini-project.Server/Features/Products/Product.cs
+++ reko-mini-project.Server/Features/Products/Product.cs
@@ -4,6 +4,7 @@ public sealed class Product
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
     public double Weight { get; set; }
     public decimal Price { get; set; }
 }
