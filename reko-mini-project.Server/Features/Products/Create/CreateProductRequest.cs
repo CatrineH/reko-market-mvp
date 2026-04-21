05dc6edd1dc848e8228430b493d78073fca3bfb4
doccelick <anders.e.bakke@gmail.com>
2026-04-21 12:43:20 +0200

    Remove CreateProductRequest and CreateProductWithImageRequest classes, and delete CreateProductWithImageEndpoint to simplify product creation process

--- reko-mini-project.Server/Features/Products/Create/CreateProductRequest.cs
+++
@@ -1,3 +0,0 @@
-namespace reko_mini_project.Server.Features.Products.Create;
-
-public sealed record CreateProductRequest(string Name, double Weight, decimal Price);
--- reko-mini-project.Server/Features/Products/Create/CreateProductWithImageRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace reko_mini_project.Server.Features.Products.Create;
-
-public sealed record CreateProductWithImageRequest(
-    string Name,
-    double Weight,
-    decimal Price,
-    IFormFile? FormFile
-    );
