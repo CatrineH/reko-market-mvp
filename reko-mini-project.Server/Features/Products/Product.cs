192f26708dfde1de6f44c5883c66abf2c012b8b0
doccelick <anders.e.bakke@gmail.com>
2026-04-21 12:40:47 +0200

    Initialize Product properties with default values for Id, Category, and Description

--- reko-mini-project.Server/Features/Products/Product.cs
+++ reko-mini-project.Server/Features/Products/Product.cs
@@ -2,8 +2,10 @@ namespace reko_mini_project.Server.Features.Products;
 
 public sealed class Product
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     public string ImageUrl { get; set; } = string.Empty;
     public double Weight { get; set; }
     public decimal Price { get; set; }
