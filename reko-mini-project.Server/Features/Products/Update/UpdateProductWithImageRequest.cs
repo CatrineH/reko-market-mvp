fe85b919843960321971c77a57079ccdc2c8bf30
doccelick <anders.e.bakke@gmail.com>
2026-04-21 12:43:01 +0200

    Remove UpdateProductRequest and UpdateProductWithImageRequest records to streamline product update process

--- reko-mini-project.Server/Features/Products/Update/UpdateProductWithImageRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace reko_mini_project.Server.Features.Products.Update;
-
-public sealed record UpdateProductWithImageRequest(
-    string Name,
-    double Weight,
-    decimal Price,
-    IFormFile? FormFile
-    );
